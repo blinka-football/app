4c281b5473f5c39f62d6797fdbf4511874b82308
Prathit <prathit.shivade@gmail.com>
2024-10-18 12:52:54 +0100

    blinka:1.0.0: router bug-fixes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Footer from './components/Footer';
 import GameSelection from './components/GameSelection';
 import GameContainer from './components/GameContainer';
 import { flashColors, stopGame } from './GameLogic';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  Navigate  // Import Navigate
+} from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/Signup';
 import ResetPassword from './components/ResetPassword';
 import PrivateRoute from './PrivateRoute';
-import ContactUs from './components/ContactUs'; 
+import ContactUs from './components/ContactUs';
 import TC from './components/TC';
-import Privacy from './components/Privacy'; 
+import Privacy from './components/Privacy';
 import { AuthContext } from './AuthProvider';
 
 const App = () => {
@@ -32,7 +38,14 @@ const MainApp = () => {
   const location = useLocation(); // Get current location
 
   // Define public routes where Header and Footer should not be displayed
-  const publicRoutes = ['/login', '/signup', '/reset-password', '/contactus', '/t&c', '/privacy'];
+  const publicRoutes = [
+    '/login',
+    '/signup',
+    '/reset-password',
+    '/contactus',
+    '/t&c',
+    '/privacy'
+  ];
 
   // Check if the current path is a public route
   const isPublicRoute = publicRoutes.includes(location.pathname);
@@ -83,41 +96,41 @@ const MainApp = () => {
 
   return (
     <>
-      {/* Conditionally render Home based on route and user authentication */}
       {!isPublicRoute && user && <Home />}
-
+  
       <Routes>
+        {/* Redirect from '/' to '/signup' using index route */}
+        <Route index element={<Navigate to="/signup" replace />} />
+  
+        {/* Public routes */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/t&c" element={<TC />} />
+        <Route path="/privacy" element={<Privacy />} />
+  
         {/* Protected routes */}
         <Route
           path="/home"
           element={
             <PrivateRoute>
-              <>
-                {!gameStarted ? (
-                  <GameSelection
-                    onSelectLevel={handleSelectLevel}
-                    onStartGame={handleStartGame}
-                    disabled={!selectedLevel}
-                    selectedLevel={selectedLevel}
-                  />
-                ) : (
-                  <GameContainer stopGame={handleStopGame} countdown={countdown} />
-                )}
-              </>
+              {!gameStarted ? (
+                <GameSelection
+                  onSelectLevel={handleSelectLevel}
+                  onStartGame={handleStartGame}
+                  disabled={!selectedLevel}
+                  selectedLevel={selectedLevel}
+                />
+              ) : (
+                <GameContainer stopGame={handleStopGame} countdown={countdown} />
+              )}
             </PrivateRoute>
           }
         />
-
-        {/* Public routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} /> 
-        <Route path="/contactus" element={<ContactUs />} /> 
-        <Route path="/t&c" element={<TC />} /> 
-        <Route path="/privacy" element={<Privacy />} /> 
+  
       </Routes>
-
-      {/* Conditionally render Footer based on current route */}
+  
       {!isPublicRoute && <Footer />}
     </>
   );
